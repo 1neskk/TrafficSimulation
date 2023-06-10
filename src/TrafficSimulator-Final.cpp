a99892fca042e8c7b53325768ba3439e940f1778
Gabriel Almeida <contato.nesk@outlook.com>
2023-06-10 11:30:20 -0300

    Minor improvement

--- src/TrafficSimulator-Final.cpp
+++ src/TrafficSimulator-Final.cpp
@@ -15,7 +15,7 @@ void createTrafficObjects_Paris(std::vector<std::shared_ptr<Street>> &streets, s
     // Note: You can use the webp format instead of jpeg
     // According to Google - WebP lossless images are 26% smaller in size compared to PNGs. 
     // WebP lossy images are 25-34% smaller than comparable JPEG images at equivalent SSIM quality index. 
-    filename = "../data/paris.jpg";
+    filename = "../data/paris.webp";
 
     // init traffic objects
     int nIntersections = 9;
@@ -58,7 +58,7 @@ void createTrafficObjects_NYC(std::vector<std::shared_ptr<Street>> &streets, std
 {
     // assign filename of corresponding city map
     // Note: You can use the webp format instead of jpeg
-    filename = "../data/nyc.jpg";
+    filename = "../data/nyc.webp";
 
     // init traffic objects
     int nIntersections = 6;
